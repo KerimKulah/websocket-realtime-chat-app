6036de7f0fa4d2545362477e12a99dc9ae892457
Kerim Külah <kerimkulah@gmail.com>
2025-05-09 06:46:41 +0300

    Real Time Chat App

---
+++ src/main/resources/static/chat.js
@@ -0,0 +1,57 @@
+function setConnected(connected) {
+    document.getElementById('sendBtn').disabled = !connected;
+}
+
+function showMessage(message) {
+    const messageArea = document.getElementById('messageArea');
+    const messageElement = document.createElement('p');
+    messageElement.textContent = message.sender + ": " + message.content;
+    messageElement.class = "mb-1";
+    messageArea.appendChild(messageElement);
+    messageArea.scrollTop = messageArea.scrollHeight;
+}
+
+function sendMessage() {
+    const sender = document.getElementById('senderInput').value;
+    const content = document.getElementById('contentInput').value;
+    const message =
+        {
+            sender: sender,
+            content: content
+        }
+    stompClient.send("/app/sendMessage", {}, JSON.stringify(message));
+    document.getElementById('contentInput').value = '';
+}
+
+function connect() {
+    const socket = new SockJS('/chat');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        stompClient.subscribe('/topic/messages', function (message) {
+            showMessage(JSON.parse(message.body));
+        });
+    })
+}
+
+const toggleBtn = document.getElementById('themeToggle');
+const htmlTag = document.getElementById('htmlTag');
+
+// Daha önce seçilmiş tema varsa uygula
+if (localStorage.getItem('theme') === 'dark') {
+    htmlTag.classList.add('dark');
+    toggleBtn.textContent = '☀️ Light Mode';
+}
+
+toggleBtn.addEventListener('click', () => {
+    htmlTag.classList.toggle('dark');
+    const isDark = htmlTag.classList.contains('dark');
+    localStorage.setItem('theme', isDark ? 'dark' : 'light');
+    toggleBtn.textContent = isDark ? '☀️ Light Mode' : '🌙 Dark Mode';
+});
+
+window.onload = () => {
+    connect();
+    document.getElementById('sendBtn').addEventListener('click', sendMessage);
+};
+
